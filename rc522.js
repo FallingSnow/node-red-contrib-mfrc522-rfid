755f5b0bd2011390dbf1adde55d62ef718591998
Ayrton Sparling <ayrton@sparling.us>
2021-06-09 21:47:25 -0700

    Actual array equality

--- rc522.js
+++ rc522.js
@@ -131,7 +131,7 @@ module.exports = function (RED) {
         card = findCard();
 
         let uidArray = getUid();
-        if (uidArray === lastUidArray && timestamp <= blockedUntil) {
+        if (uidArray.equals(lastUidArray) && timestamp <= blockedUntil) {
           throw new Error("Blocked");
         }
 
@@ -188,3 +188,35 @@ module.exports = function (RED) {
 
   RED.nodes.registerType("rc522", rc522);
 }
+
+
+// https://stackoverflow.com/questions/7837456/how-to-compare-arrays-in-javascript
+// Warn if overriding existing method
+if(Array.prototype.equals)
+    console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
+// attach the .equals method to Array's prototype to call it on any array
+Array.prototype.equals = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+
+    // compare lengths - can save a lot of time 
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].equals(array[i]))
+                return false;       
+        }           
+        else if (this[i] != array[i]) { 
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;   
+        }           
+    }       
+    return true;
+}
+// Hide method from for-in loops
+Object.defineProperty(Array.prototype, "equals", {enumerable: false});
